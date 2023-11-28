ac02860e760eef18ca4a3a1f5b9558e0044021d5
Pedro Henrique Serafini <111706609+PedroSerafini@users.noreply.github.com>
2023-11-28 20:49:49 -0300

    First commit

---
+++ dashboard-veterinaria/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+file_path = 'BancodeDadosA3_20231122152427.csv'
+df = pd.read_csv(file_path, thousands=',')
+
+st.title('Dashboard de Diagnóstico Médico')
+
+fig_hemoglobina = px.bar(df, x='Diagnóstico', y='Hemoglobina (g/dL)', title='Hemoglobina por Diagnóstico')
+st.plotly_chart(fig_hemoglobina)
+
+fig_linfocito = px.bar(df, x='Diagnóstico', y='Linfócitos (/µL)', title='Linfócitos por Diagnóstico')
+st.plotly_chart(fig_linfocito)
+
+st.subheader('Dados Originais')
+st.write(df)
