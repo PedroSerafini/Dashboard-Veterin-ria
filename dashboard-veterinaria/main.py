e704fba9438349d570a9e747fdb74aa28ae668f1
Pedro Henrique Serafini <111706609+PedroSerafini@users.noreply.github.com>
2023-12-04 17:50:16 -0300

    Update main.py

--- dashboard-veterinaria/main.py
+++ dashboard-veterinaria/main.py
@@ -2,6 +2,13 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+uploaded_file = st.file_uploader(
+    "Choose your database", accept_multiple_files=False)
+if uploaded_file is not None:
+    file_path = uploaded_file
+else:
+    file_path = "BancodeDadosA3_20231122152427.csv"
+
 file_path = 'BancodeDadosA3_20231122152427.csv'
 df = pd.read_csv(file_path, thousands=',')
 
